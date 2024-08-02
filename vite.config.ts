3af89c705f42cacb7862826386221b006d498dff
Anton Tkachuk <tkachuk.ant@mail.ru>
2024-08-03 00:33:07 +0300

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/gantt-diagram-app',
+  plugins: [ react() ],
 })
